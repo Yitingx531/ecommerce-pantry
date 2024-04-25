a5e4ed04c1e28c8e11655c77a67dfcb5969845a9
Yiting Xiao <yitingxiao0531@gmail.com>
2024-04-24 22:21:45 -0400

    post a new user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { userRouter } from './routes/user'
 
 const app: Express = express();
 const PORT = 3001;
@@ -8,9 +9,11 @@ const PORT = 3001;
 app.use(express.json());
 app.use(cors());
 
+/* localhost:3001/user/register */
+app.use('/user', userRouter);
 
 mongoose.connect(
-    "mongodb+srv://yitingxiao0531:DnuzhtdqZ4cbkXNx@ecommerce.px2cuwp.mongodb.net/?retryWrites=true&w=majority&appName=ECommerce"
+    'mongodb+srv://yitingxiao0531:DnuzhtdqZ4cbkXNx@ecommerce.px2cuwp.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=ECommerce'
 );
 
 
--- src/models/user.ts
+++ src/models/user.ts
@@ -7,3 +7,12 @@ export interface IUser {
     availableMoney: number;
     // purchasedItems: string[];
 }
+
+const UserSchema = new Schema<IUser>({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: true },
+    availableMoney: { type: Number, default: 5000 },
+    // purchasedItems:
+});
+
+export const UserModel = model<IUser>('user', UserSchema);
